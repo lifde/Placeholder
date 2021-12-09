f054f51e8c7c2400413ce4bbc928b64206894ed5
Calixte <obissan.suaudeau@gmail.com>
2021-12-09 16:39:53 +0100

    first push

---
+++ main.js
@@ -0,0 +1,31 @@
+var $status = document.getElementById('status');
+
+if ('Notification' in window) {
+    $status.innerText = Notification.permission;
+}
+
+function requestPermission() {
+    if (!('Notification' in window)) {
+        alert('Notification API not supported!');
+        return;
+    }
+
+    Notification.requestPermission(function (result) {
+        $status.innerText = result;
+    });
+}
+
+function persistentNotification() {
+    if (!('Notification' in window) || !('ServiceWorkerRegistration' in window)) {
+        alert('Persistent Notification API not supported!');
+        return;
+    }
+
+    try {
+        navigator.serviceWorker.getRegistration()
+        .then((reg) => reg.showNotification("Hi there - persistent!"))
+        .catch((err) => alert('Service Worker registration error: ' + err));
+    } catch (err) {
+        alert('Notification API error: ' + err);
+    }
+}

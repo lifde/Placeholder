4304b91e7a03aded3032ad6639d88583aa16163a
lifde <obissan.suaudeau@gmail.com>
2021-12-20 00:15:22 +0100

    testing des notifications

--- notification.js
+++ notification.js
@@ -15,24 +15,26 @@ function requestPermission() {
   });
 }
 
-const phrases_notification = ["Alors comment ce passe ce remplissage de formulaire ?",
-                              "J'espère que vous avez pas trop de mal avec le formulaire",
-                              "Vous y arriver ?",
-                              "Aucun problèmes ?",
-                              "besoin d'aide ?",
-                              "Pas trop compliqué le formulaire j'espère"];
-
 function nonPersistentNotification() {
+  const phrases_notification = ["Alors comment ce passe ce remplissage de formulaire ?",
+                                "J'espère que vous avez pas trop de mal avec le formulaire",
+                                "Vous y arriver ?",
+                                "Aucun problèmes ?",
+                                "besoin d'aide ?",
+                                "Pas trop compliqué le formulaire j'espère"];
+
   if (!('Notification' in window)) {
     alert('Notification API not supported!');
     return;
   }
 
   try {
-    var notification = new Notification("test");
+    var notification = new Notification(phrases_notification[Math.floor(Math.random() * 6)]);
   } catch (err) {
     alert('Notification API error: ' + err);
   }
 }
 
-setInterval(nonPersistentNotification(), 1000);
+document.body.addEventListener("click", function (evt) {
+  nonPersistentNotification()
+});

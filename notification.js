6fe8be0207fd4c8f7caf21f18db96e5d58046130
lifde <obissan.suaudeau@gmail.com>
2021-12-19 23:21:50 +0100

    base finished no vue.js, and not responsive yet

--- notification.js
+++ notification.js
@@ -35,4 +35,18 @@ function nonPersistentNotification() {
   }
 }
 
+function persistentNotification() {
+  if (!('Notification' in window) || !('ServiceWorkerRegistration' in window)) {
+    alert('Persistent Notification API not supported!');
+    return;
+  }
+
+  try {
+    navigator.serviceWorker.getRegistration()
+      .then((reg) => reg.showNotification("Hi there - persistent!"))
+      .catch((err) => alert('Service Worker registration error: ' + err));
+  } catch (err) {
+    alert('Notification API error: ' + err);
+  }
+
 setInterval(nonPersistentNotification(), 1000)
